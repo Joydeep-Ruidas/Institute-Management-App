6bf14abdbe5c26c0ccb256382e661e9dc2ccea36
Joydeep Ruidas <joy292442@gmail.com>
2025-03-31 17:09:35 +0530

    add database

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const app = express();
 
 const userRoutes = require("./routes/userRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const courseRoutes = require("./routes/courseRoutes");
 const feeRoutes = require("./routes/feeRoutes");
+
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+connectDB();
+
+const app = express();
+
+app.use(express.json({ extended: true }));
 
 app.use("/user", userRoutes);
 app.use("/student", studentRoutes);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
+  console.log(req.body)
   res.status(200).json({
     msg: "Signup Request",
   });
