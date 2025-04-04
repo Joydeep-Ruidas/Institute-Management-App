364bba1cfa74803d65c9fa0d6309c93b18c9c84e
Joydeep Ruidas <joy292442@gmail.com>
2025-04-04 13:23:05 +0530

    seperate course routes in courseRoutes and course logic in courseController

---
+++ backend/controllers/courseControllers.js
@@ -0,0 +1,177 @@
+const courseModel = require("../models/courseModel");
+const mongoose = require("mongoose");
+const cloudinary = require("../config/cloudinaryConfig");
+const studentModel = require("../models/studentModel");
+
+const addCourse = async (req, res) => {
+  try {
+    const { image } = req.files;
+
+    const { secure_url: imageURL, public_id: imageID } =
+      await cloudinary.uploader.upload(image.tempFilePath, {
+        folder: "Institute_Management_App",
+      });
+
+    const { courseName, price, description, startingDate, endDate } = req.body;
+    const { uID } = req.user;
+
+    const newCourse = new courseModel({
+      _id: new mongoose.Types.ObjectId(),
+      courseName,
+      price,
+      description,
+      startingDate,
+      endDate,
+      imageID,
+      imageURL,
+      uID,
+    });
+
+    const saveCourse = await newCourse.save();
+
+    res.status(201).json({ saveCourse });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const allCourses = async (req, res) => {
+  try {
+    const { uID } = req.user;
+
+    const allCourses = await courseModel.find({ uID }).sort({ $natural: -1 });
+
+    res.status(200).json({ allCourses });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Get all the student details in any one course and also course details for any particular User
+const courseDetails = async (req, res) => {
+  try {
+    const courseDetails = await courseModel.findById(req.params.id);
+
+    const studentDetails = await studentModel.find({ courseID: req.params.id });
+    res.status(200).json({ courseDetails, studentDetails });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Get latest 5 courses for any particular User
+const latestCourses = async (req, res) => {
+  try {
+    const { uID } = req.user;
+
+    const courses = await courseModel
+      .find({ uID })
+      .sort({ $natural: -1 })
+      .limit(5);
+
+    res.status(200).json({ courses });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const updateCourse = async (req, res) => {
+  try {
+    const { uID } = req.user;
+    const { courseName, price, description, startingDate, endDate } = req.body;
+
+    const matchCourse = await courseModel.findById(req.params.id);
+
+    if (matchCourse.uID !== uID) {
+      return res
+        .status(401)
+        .json({ msg: "You are not authorized to update this course" });
+    }
+
+    if (req.files) {
+      await cloudinary.uploader.destroy(matchCourse.imageID);
+
+      const { image } = req.files;
+
+      const { secure_url: imageURL, public_id: imageID } =
+        await cloudinary.uploader.upload(image.tempFilePath, {
+          folder: "Institute_Management_App",
+        });
+
+      const updatedCourseData = {
+        courseName,
+        price,
+        description,
+        startingDate,
+        endDate,
+        imageID,
+        imageURL,
+        uID,
+      };
+
+      const updatedCourse = await courseModel.findByIdAndUpdate(
+        req.params.id,
+        updatedCourseData,
+        { new: true }
+      );
+
+      res.status(202).json({ updatedCourse });
+    } else {
+      const updatedCourseData = {
+        courseName,
+        price,
+        description,
+        startingDate,
+        endDate,
+        imageID: matchCourse.imageID,
+        imageURL: matchCourse.imageURL,
+        uID,
+      };
+
+      const updatedCourse = await courseModel.findByIdAndUpdate(
+        req.params.id,
+        updatedCourseData,
+        { new: true }
+      );
+
+      res.status(202).json({ updatedCourse });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const deleteCourse = async (req, res) => {
+  try {
+    const matchCourse = await courseModel.findById(req.params.id);
+    const { uID } = req.user;
+
+    if (matchCourse.uID !== uID) {
+      res
+        .status(401)
+        .json({ msg: "You are not authorized to delete this post" });
+    } else {
+      await courseModel.findByIdAndDelete(req.params.id);
+      await cloudinary.uploader.destroy(matchCourse.imageID);
+
+      res.status(202).json({ msg: "Course deleted successfully" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = {
+  addCourse,
+  allCourses,
+  latestCourses,
+  courseDetails,
+  updateCourse,
+  deleteCourse
+};
--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,176 +1,20 @@
 const express = require("express");
-const checkAuth = require("../middleware/checkAuth");
 const router = express.Router();
-const course = require("../models/courseModel");
-const mongoose = require("mongoose");
-const cloudinary = require("../config/cloudinaryConfig");
-const studentModel=require("../models/studentModel")
+const checkAuth = require("../middleware/checkAuth");
+const {
+  addCourse,
+  allCourses,
+  latestCourses,
+  courseDetails,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courseControllers");
 
-router.post("/add-course", checkAuth, async (req, res) => {
-  try {
-    const { image } = req.files;
-
-    const { secure_url: imageURL, public_id: imageID } =
-      await cloudinary.uploader.upload(image.tempFilePath, {
-        folder: "Institute_Management_App",
-      });
-
-    const { courseName, price, description, startingDate, endDate } = req.body;
-    const { uID } = req.user; // Assuming checkAuth attaches user info to req
-
-    const newCourse = new course({
-      _id: new mongoose.Types.ObjectId(),
-      courseName,
-      price,
-      description,
-      startingDate,
-      endDate,
-      imageID,
-      imageURL,
-      uID,
-    });
-
-    const saveCourse = await newCourse.save();
-
-    res.status(201).json({ newCourse: saveCourse });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-
-// Get latest 5 courses for any particular User
-router.get("/latest-course", checkAuth, async (req, res) => {
-  try {
-    const { uID } = req.user;
-
-    const courses = await course
-      .find({ uID })
-      .sort({ $natural: -1 })
-      .limit(5);
-
-    res.status(200).json({ courses });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ msg: err.message });
-  }
-});
-
-// Get all course for any particular User
-router.get("/", checkAuth, async (req, res) => {
-  try {
-    const { uID } = req.user;
-
-    const allCourses = await course.find({ uID });
-
-    res.status(200).json({ allCourses });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// Get one course details for any particular User
-router.get("/course-details/:id", checkAuth, async (req, res) => {
-  try {
-    const courseDetails = await course.findById(req.params.id);
-
-    const studentDetails = await studentModel.find({courseID:req.params.id})
-    res.status(200).json({ courseDetails, studentDetails });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.put("/update-course/:id", checkAuth, async (req, res) => {
-  try {
-    const { uID } = req.user;
-    const { courseName, price, description, startingDate, endDate } = req.body;
-
-    const matchCourse = await course.findById(req.params.id);
-
-    if (matchCourse.uID !== uID) {
-      return res
-        .status(401)
-        .json({ msg: "You are not authorized to update this course" });
-    }
-
-    if (req.files) {
-      await cloudinary.uploader.destroy(matchCourse.imageID);
-
-      const { image } = req.files;
-
-      const { secure_url: imageURL, public_id: imageID } =
-        await cloudinary.uploader.upload(image.tempFilePath, {
-          folder: "Institute_Management_App",
-        });
-
-      const updatedCourseData = {
-        courseName,
-        price,
-        description,
-        startingDate,
-        endDate,
-        imageID,
-        imageURL,
-        uID,
-      };
-
-      const updatedCourse = await course.findByIdAndUpdate(
-        req.params.id,
-        updatedCourseData,
-        { new: true }
-      );
-
-      res.status(200).json({ updatedCourse });
-    } else {
-      const updatedCourseData = {
-        courseName,
-        price,
-        description,
-        startingDate,
-        endDate,
-        imageID: matchCourse.imageID,
-        imageURL: matchCourse.imageURL,
-        uID,
-      };
-
-      const updatedCourse = await course.findByIdAndUpdate(
-        req.params.id,
-        updatedCourseData,
-        { new: true }
-      );
-
-      res.status(200).json({ updatedCourse });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.delete("/delete-course/:id", checkAuth, async (req, res) => {
-  try {
-    const matchCourse = await course.findById(req.params.id);
-    const { uID } = req.user;
-
-    if (matchCourse.uID === uID) {
-      await course.findByIdAndDelete(req.params.id);
-      await cloudinary.uploader.destroy(matchCourse.imageID);
-
-      res.status(200).json({ msg: "Course deleted successfully" });
-    } else {
-      res
-        .status(401)
-        .json({ msg: "You are not authorized to delete this post" });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
+router.post("/add-course", checkAuth, addCourse);
+router.get("/all-courses", checkAuth, allCourses);
+router.get("/course-details/:id", checkAuth, courseDetails); // Get all the student details in any one course and also course details for any particular User
+router.get("/latest-courses", checkAuth, latestCourses); // Get latest 5 courses for any particular User
+router.put("/update-course/:id", checkAuth, updateCourse);
+router.delete("/delete-course/:id", checkAuth, deleteCourse);
 
 module.exports = router;
